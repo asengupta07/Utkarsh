bdaf7f40a399e730ff49527e8e3ce85684db7a50
Sachindra Kumar Singh <ssachinsingh99@gmail.com>
2023-10-31 00:17:57 +0530

    vertical rule

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,8 +14,9 @@ const Navbar = () =>{
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-auto nas">
+    <div className="vr"></div>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item me-4">
           <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
