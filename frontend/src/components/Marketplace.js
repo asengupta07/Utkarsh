e18710dc3a6e98079d78ab81439428a32c4c67ba
Sachindra Kumar Singh <ssachinsingh99@gmail.com>
2023-11-04 16:05:14 +0530

    css changes

--- frontend/src/components/Marketplace.js
+++ frontend/src/components/Marketplace.js
@@ -42,7 +42,7 @@ const Marketplace = () => {
           <div class="row">
             <div class="col pb-3">
               <div class="card">
-                <img src="https://cdn.discordapp.com/attachments/1169346758636224614/1169349058062733332/IMG-20231101-WA0076.jpg?ex=6555142c&is=65429f2c&hm=cd00e1d8471b4d2c2eaf69d75888447ee3c9997914b139ba996c303ca867f9e8&" class="card-img-top" alt="" />
+                <img src="https://cdn.discordapp.com/attachments/1169346758636224614/1169349058062733332/IMG-20231101-WA0076.jpg?ex=6555142c&is=65429f2c&hm=cd00e1d8471b4d2c2eaf69d75888447ee3c9997914b139ba996c303ca867f9e8&" class="card-img-top sqrmage" alt="" />
                 <div class="card-body">
                   <h5 class="card-title">Teracotta Bag</h5>
                   <p class="card-text">₹ 49.00</p>
@@ -52,7 +52,7 @@ const Marketplace = () => {
             </div>
             <div class="col pb-3">
               <div class="card">
-                <img src="https://cdn.discordapp.com/attachments/1169346758636224614/1169347125780434994/IMG-20231101-WA0074.jpg?ex=65551260&is=65429d60&hm=c5979a4a9844ca3f4724407d08b4234cb5c660daec3e2193ac66fddbf0c03af8&" class="card-img-top" alt="" />
+                <img src="https://cdn.discordapp.com/attachments/1169346758636224614/1169347125780434994/IMG-20231101-WA0074.jpg?ex=65551260&is=65429d60&hm=c5979a4a9844ca3f4724407d08b4234cb5c660daec3e2193ac66fddbf0c03af8&" class="card-img-top sqrmage" alt="" />
                 <div class="card-body">
                   <h5 class="card-title">Blue Vase Set</h5>
                   <p class="card-text">₹ 199.00</p>
@@ -62,7 +62,7 @@ const Marketplace = () => {
             </div>
             <div class="col pb-3">
               <div class="card">
-                <img src="https://cdn.discordapp.com/attachments/1169346758636224614/1169349058792542389/IMG-20231102-WA0006.jpg?ex=6555142d&is=65429f2d&hm=9e522ae041fb04a1b628607273414b580483ddd22ecab13f823002c56cb4a3f9&" class="card-img-top" alt="" />
+                <img src="https://cdn.discordapp.com/attachments/1169346758636224614/1169349058792542389/IMG-20231102-WA0006.jpg?ex=6555142d&is=65429f2d&hm=9e522ae041fb04a1b628607273414b580483ddd22ecab13f823002c56cb4a3f9&" class="card-img-top sqrmage" alt="" />
                 <div class="card-body">
                   <h5 class="card-title">Saree</h5>
                   <p class="card-text">₹ 169.00</p>
@@ -72,7 +72,7 @@ const Marketplace = () => {
             </div>
             <div class="col pb-3">
               <div class="card">
-                <img src="https://cdn.discordapp.com/attachments/1169346758636224614/1169349059144855672/IMG-20231102-WA0003.jpg?ex=6555142d&is=65429f2d&hm=611cfbf808ad241a73ba08538a6c11b4a5240d9170c3941e9ba3138b97887c46&" class="card-img-top" alt="" />
+                <img src="https://cdn.discordapp.com/attachments/1169346758636224614/1169349059144855672/IMG-20231102-WA0003.jpg?ex=6555142d&is=65429f2d&hm=611cfbf808ad241a73ba08538a6c11b4a5240d9170c3941e9ba3138b97887c46&" class="card-img-top sqrmage" alt="" />
                 <div class="card-body">
                   <h5 class="card-title">Bedsheet</h5>
                   <p class="card-text">₹ 129.00</p>
@@ -88,7 +88,7 @@ const Marketplace = () => {
               data1.products.map((getProd, index) => (
                 <div key={index} className="col pb-3">
                   <div className="card">
-                    <img src={'http://localhost:8000/backend/' + getProd.image.substring(9)} className="card-img-top" alt="" />
+                    <img src={'http://localhost:8000/backend/' + getProd.image.substring(9)} className="card-img-top sqrmage" alt="" />
                     <div className="card-body">
                       <h5 className="card-title">{getProd.name}</h5>
                       <p className="card-text">₹ {getProd.price}</p>
